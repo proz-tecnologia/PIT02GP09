dccd486beec2ed229cabd610117bec7f8d78307b
wood <fabio.wood.rs@gmail.com>
2022-12-21 22:08:17 -0300

    correction_apprepository_now_is_abstract_class

--- lib/src/shared/repositories/app_repository.dart
+++ lib/src/shared/repositories/app_repository.dart
@@ -1,11 +1,7 @@
-
-import 'dart:convert';
-
 import 'package:projeto_gestao_financeira_grupo_nove/src/shared/models/user_model.dart';
 import 'package:projeto_gestao_financeira_grupo_nove/src/shared/repositories/repository.dart';
-import 'package:projeto_gestao_financeira_grupo_nove/src/shared/utils/shared_preferences_keys.dart';
 
-class AppRepository extends Repository {
+abstract class AppRepository extends Repository {
 
   List<UserModel> usersLogin = <UserModel>[];
 
@@ -13,37 +9,9 @@ class AppRepository extends Repository {
     init();
   }
 
-  Future<void> init() async {
-    final users = sharedPreferences.getString(SharedPreferencesKeys.users);
-
-    if (users != null && users.isNotEmpty) {
-      final usersDecode = jsonDecode(users);
-
-      final decodedUsers =
-          (usersDecode as List).map((e) => UserModel.fromJson(e)).toList();
-      usersLogin.addAll(decodedUsers);
-    }
-  }
-
-  Future<List<UserModel>> getUser() async {
-    final users = sharedPreferences.getString(SharedPreferencesKeys.users);
-
-    if (users != null && users.isNotEmpty) {
-      final usersDecode = jsonDecode(users);
-
-      final decodedUsers =
-          (usersDecode as List).map((e) => UserModel.fromJson(e)).toList();
-      usersLogin.addAll(decodedUsers);
-    }
-
-    return usersLogin;
-  }
-
-  Future<String> getUserSession() async {
-    return sharedPreferences
-        .getString(SharedPreferencesKeys.userSession)
-        .toString();
-  }
+  Future<void> init();
+  Future<List<UserModel>> getUser();
+  Future<String> getUserSession();
 
   
 }
