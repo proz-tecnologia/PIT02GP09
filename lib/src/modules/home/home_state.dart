80f49388735b2303f23ec7e7c76bf3285f2561ac
wood <fabio.wood.rs@gmail.com>
2022-12-26 18:46:20 -0300

    home_repository_impl_getUserData_search_corrected

--- lib/src/modules/home/home_state.dart
+++ lib/src/modules/home/home_state.dart
@@ -1,9 +1,25 @@
+import 'package:projeto_gestao_financeira_grupo_nove/src/shared/models/user_model.dart';
+
 abstract class HomeState {}
 
 class HomeStateEmpty extends HomeState {}
 
 class HomeStateLoading extends HomeState {}
 
-class HomeStateSuccess extends HomeState {}
+class HomeStateSuccess extends HomeState {
+  final UserModel user;
+  
+  HomeStateSuccess({
+    required this.user,
+  });
+  
+}
 
-class HomeStateError extends HomeState {}
+class HomeStateError extends HomeState {
+  final Object erro;
+
+  HomeStateError({required this.erro});
+
+  @override
+  String toString() => 'HomeStateError(erro: $erro)';
+}
