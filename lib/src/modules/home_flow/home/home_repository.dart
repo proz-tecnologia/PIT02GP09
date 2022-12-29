36ed0252f3c5e360ec4b43c62fe10a08e4c1cab9
wood <fabio.wood.rs@gmail.com>
2022-12-29 16:29:49 -0300

    home_repositories_getTransactions_func_created

--- lib/src/modules/home_flow/home/home_repository.dart
+++ lib/src/modules/home_flow/home/home_repository.dart
@@ -1,3 +1,4 @@
+import 'package:projeto_gestao_financeira_grupo_nove/src/shared/models/financial_transactions/financial_transaction.dart';
 import 'package:projeto_gestao_financeira_grupo_nove/src/shared/models/user_model.dart';
 import 'package:projeto_gestao_financeira_grupo_nove/src/shared/repositories/repository.dart';
 
@@ -6,5 +7,6 @@ abstract class HomePageRepository extends Repository {
   HomePageRepository({required super.sharedPreferences});
 
   Future<UserModel> getUserData({required String userID});
+  Future<List<FinancialTransaction>> getTransactions({required String userID});
 
 }
--- lib/src/modules/home_flow/home/home_repository_impl.dart
+++ lib/src/modules/home_flow/home/home_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:projeto_gestao_financeira_grupo_nove/src/modules/home_flow/home/home_repository.dart';
+import 'package:projeto_gestao_financeira_grupo_nove/src/shared/models/financial_transactions/financial_transaction.dart';
 import 'package:projeto_gestao_financeira_grupo_nove/src/shared/models/user_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -10,6 +11,8 @@ class HomePageRepositoryImpl implements HomePageRepository {
   final SharedPreferences sharedPreferences;
 
   HomePageRepositoryImpl({required this.sharedPreferences});
+
+  FirebaseFirestore get _firestore => FirebaseFirestore.instance;
 
   @override
   Future<UserModel> getUserData({required String userID}) async {
@@ -22,6 +25,19 @@ class HomePageRepositoryImpl implements HomePageRepository {
     final userData = UserModel.fromMap(document);
     log(userData.userModelID);
     return userData;
+  }
+
+  @override
+  Future<List<FinancialTransaction>> getTransactions({required String userID}) async {
+    final firebaseTransactions = 
+    await _firestore
+    .collection('transactions')
+    .where('userID', isEqualTo: userID )
+    .orderBy('date')
+    .get();
+
+    final transactions = firebaseTransactions.docs.map((e) => FinancialTransaction.fromMap(e.data())).toList();
+    return transactions;
   }
   
 }
