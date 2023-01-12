7d22c645c3809e7ca2c05ca5965b1fa0f0c16723
wood <fabio.wood.rs@gmail.com>
2023-01-12 16:03:09 -0300

    formatters_now_with_formatToDate_function

--- lib/src/shared/utils/formatters.dart
+++ lib/src/shared/utils/formatters.dart
@@ -1,9 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 
 class Formatters {
 
-  static String formatToReal(double value ){
+  static String formatToReal(double value ) {
     String formatedValue = value.toStringAsFixed(2).replaceFirst(".", ",");
     return "R\$ $formatedValue";
+  }
+
+  static String formatToDate(Timestamp value) {
+    final formattedValue = DateFormat('dd/MM/yyyy').format(value.toDate());
+    return formattedValue;
   }
 
 }
