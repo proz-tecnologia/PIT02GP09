97082ee9373344f4b21be64b21817d495a6bc41a
wood <fabio.wood.rs@gmail.com>
2023-01-12 15:14:03 -0300

    planningModel_now_with_userID_parameter

--- lib/src/shared/models/plannings/planning_model.dart
+++ lib/src/shared/models/plannings/planning_model.dart
@@ -9,10 +9,12 @@ class PlanningModel {
   final String name;
   final double value;
   final Timestamp finalDate;
+  final String? userID;
   PlanningModel({
     required this.name,
     required this.value,
     required this.finalDate,
+    required this.userID,
   });
 
    String get formattedDate => DateFormat('dd/MM/yyyy').format(finalDate.toDate());  
@@ -21,11 +23,13 @@ class PlanningModel {
     String? name,
     double? value,
     Timestamp? finalDate,
+    String? userID,
   }) {
     return PlanningModel(
       name: name ?? this.name,
       value: value ?? this.value,
       finalDate: finalDate ?? this.finalDate,
+      userID: userID ?? this.userID,
     );
   }
 
@@ -34,6 +38,7 @@ class PlanningModel {
       'name': name,
       'value': value,
       'finalDate': finalDate,
+      'userID' : userID,
     };
   }
 
@@ -42,6 +47,7 @@ class PlanningModel {
       name: map['name'] as String,
       value: map['value'] as double,
       finalDate: map['finalDate'],
+      userID: map['userID'] != null ? map['userID'] as String : null,
     );
   }
 
@@ -50,7 +56,7 @@ class PlanningModel {
   factory PlanningModel.fromJson(String source) => PlanningModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() => 'PlanningModel(name: $name, value: $value, finalDate: $finalDate)';
+  String toString() => 'PlanningModel(name: $name, value: $value, finalDate: $finalDate, userID: $userID)';
 
   @override
   bool operator ==(covariant PlanningModel other) {
@@ -59,9 +65,10 @@ class PlanningModel {
     return 
       other.name == name &&
       other.value == value &&
-      other.finalDate == finalDate;
+      other.finalDate == finalDate &&
+      other.userID == userID;
   }
 
   @override
-  int get hashCode => name.hashCode ^ value.hashCode ^ finalDate.hashCode;
+  int get hashCode => name.hashCode ^ value.hashCode ^ finalDate.hashCode ^ userID.hashCode;
 }
