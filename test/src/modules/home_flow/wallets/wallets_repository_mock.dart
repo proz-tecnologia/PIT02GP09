61cd5e9fb3a0b76982adec8ed1e4cb408f6c14ad
wood <fabio.wood.rs@gmail.com>
2023-01-21 17:06:16 -0300

    wallets_repository_fake_test

---
+++ test/src/modules/home_flow/wallets/wallets_repository_mock.dart
@@ -0,0 +1,5 @@
+
+import 'package:mocktail/mocktail.dart';
+import 'package:projeto_gestao_financeira_grupo_nove/src/modules/home_flow/wallets/wallets_repository.impl.dart';
+
+class WalletsRepositoryMock extends Mock implements WalletsRepositoryImpl {}
