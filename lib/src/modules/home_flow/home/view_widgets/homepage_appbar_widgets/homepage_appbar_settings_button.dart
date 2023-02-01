1a218201caa0a56bad56f89f27120a62ef3d5e0f
wood <fabio.wood.rs@gmail.com>
2023-02-01 19:57:11 -0300

    homepage_appbar_button_now_logout

--- lib/src/modules/home_flow/home/view_widgets/homepage_appbar_widgets/homepage_appbar_settings_button.dart
+++ lib/src/modules/home_flow/home/view_widgets/homepage_appbar_widgets/homepage_appbar_settings_button.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:projeto_gestao_financeira_grupo_nove/src/modules/home_flow/home/home_bloc.dart';
+import 'package:projeto_gestao_financeira_grupo_nove/src/modules/home_flow/home/home_event.dart';
+import 'package:projeto_gestao_financeira_grupo_nove/src/routes/consts_routes.dart';
 
 class HomepageAppBarSettingsButton extends StatelessWidget {
   const HomepageAppBarSettingsButton({super.key});
@@ -7,18 +11,21 @@ class HomepageAppBarSettingsButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
             minimum: const EdgeInsets.only(right: 15.0),
-            child: IconButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                  context, 
-                  '/settings'
-                  );
-              }, 
-              icon: const Icon(
-                size: 40.0,
-                Icons.settings,
-                ),
-              ),
+            child: Column(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Modular.get<HomeBloc>().add(OnHomePageLogout());
+                    Modular.to.navigate(ConstsRoutes.signUpPage);
+                  }, 
+                  icon: const Icon(
+                    size: 40.0,
+                    Icons.logout,
+                    ),
+                  ),
+                  const Text('Logout'),
+              ],
+            ),
             );
   }
 }
